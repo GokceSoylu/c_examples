59b05e0b0ab8524e86922eec0dc373333ccb9d88
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-02-21 23:51:19 +0300

    .

---
+++ kolaySoru.c
@@ -0,0 +1,28 @@
+/*rakamları toplamı kendisini veren satı 145=1!+4!+5! kolay soru;) */
+#include <stdio.h>
+int faktoriyel(int);
+int main()
+{
+    int n,a,integer,sum=0;
+    printf("kontrol etmek istediginiz sayıyı giriniz lutfen");
+    scanf("%d",&n);
+    a=n;
+    while(a>0)
+    {
+        integer=a%10;
+        sum+=faktoriyel(integer);
+        a=a/10;
+    }
+    if(sum==n)
+        printf("iste bu sayınızın rakamlarınıın faktoriyelleri toplamı sayınıza esit");
+    else
+        printf("maşesef sayınızın rakamlarınınn faktoriyelleri toplamı sayınıza est degildir");
+    return 0;
+}
+int faktoriyel (int x)
+{
+    if(x==0)
+        return 1;
+    else
+        return x*faktoriyel(x-1);
+}
