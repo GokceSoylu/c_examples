fc3dc88c190f3068a5b3cb22787869ed300ebb56
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-03-02 11:23:59 +0300

    .

--- pointers/tryForPointers2.c
+++ pointers/tryForPointers2.c
@@ -1,10 +1,12 @@
-/* pointers öğrenmek adına denemeler */
+/* pointers öğrenmek adına denemeler 
+dikkat! yüksek oranda hayal içerir --> pointerslari işaret parmaklarının ucunda yıldız olan  insanlar(işarteçiler)dir bu işaretcileri * ile çagirirsan
+bu yenetenekleri aktifleşir ve o parmaklarının ucundaki yıldzı sayesinde bildikleri adresteki kisinin içini gösterirler*/
 #include <stdio.h>
 int main()
 {
     int *m,**n,***p;
     int a=8;
-    m=&a;
+    m=&a;//burada m e a nın adresini öğrettik *m yaptığımızda a nın içinği gösterir ;))
     n=&m;
     p=&n;
     printf("\na= %d  *m= %d  **n= %d  ****p= %d\n",a,*m,**n,***p);
@@ -12,4 +14,5 @@ int main()
     printf("\n&m= %d  n= %d  **p= %d\n",&m,n,*p);
     printf("\n&n= %d  *p= %d\n",&n,p);
     return 0;
-}
+}/*mesela a için degisiklik yapmak istersende m in basina * koyarak yapabililrsin çünkü basına yıldız koydugunda m yıldıız sayesinde a nın içine iiişk 
+tutar ve sende istediğin degisikliği yapabilirsin --> *m+=7; dersek a=15 olur :)*/
