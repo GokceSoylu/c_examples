d1c2df548551af34b46aa5cd529d37e465c83b9e
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-04-07 23:37:58 +0300

    .

--- arrayFrekans.c
+++ arrayFrekans.c
@@ -1,25 +1,25 @@
-/* bir dizini elemanlarının frekansını bulmak için şimdiye kadra çeşitli yontemler denedik simdi yeni birtane */
+/* bir dizini elemanlarının frekansını bulmak için şimdiye kadar çeşitli yontemler denedik simdi yeni birtane */
 #include <stdio.h>
-int f(int*);
+void f(int*, int*, int);
 int main()
 {
-    int a[28]={1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7},i,j,b[28]={0},size=28;
-    for(i=0;i<28;i++)
-        for(j=i;j<28;j++)
+    int a[28]={2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7,0},i,j,b[28]={0},size=28;
+    for(i=0;a[i]!=0;i++)
+        for(j=i+1;j<size;j++)
             if(a[i]==a[j])
             {
                 b[i]++;
-                size=f(a,size,i);
+                f(a,&size,j);
+                j--;
             }    
-    for(i=0;i<28;i++)
-        printf("\n%d adet %d bulunmaktadır",b[i],i);
+    for(i=0;i<size;i++)
+        printf("\n%d adet %d bulunmaktadır",b[i]+1,a[i]);
     return 0;
 }
-int f(int *a, int size, int i)
+void f(int *a, int *size, int j)//eleman kaydırma 
 {
-    for(k=i;k<size;k++)
-    {
-        a[k]=a[k+1];
-    }    
-    return size-1;
+    int k;
+    for(k=j;k<*size;k++)
+        a[k]=a[k+1]; 
+    *size-=1;
 }
