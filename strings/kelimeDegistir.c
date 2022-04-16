ec6bee0c142b49e1e3975c3ca83d60aa08aeb864
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-04-16 18:03:45 +0300

    .

--- strings/kelimeDegistir.c
+++ strings/kelimeDegistir.c
@@ -1,9 +1,26 @@
-/*cumlede istenen kelimeyi istenen kelimeyi istenen diger kelimeyle degistirir */
+/*cumlede istenen kelimeyi istediginiz baska bir kelimeyle degistiren c programına bakmaktasınız ;) */
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-    int i;
-    char array[50],kelime[20],kelime2[20];
-    
+    int i=0,j;
+    char array[50],kelime[20],kelime2[20],try[20];
+    printf("\ncumleyi giriniz lutfen ");
+    gets(array);
+    printf("\ndegistiremek istediginiz kelime: ");
+    gets(kelime);
+    printf("\nyerine yazılacak kelime ");
+    gets(kelime2);
+    while(array[i])
+    {
+        for(j=0;j<strlen(kelime);j++)
+            try[j]=array[i+j];
+        if(strcmp(try,kelime)==0)
+            for(j=0;j<strlen(kelime);j++)
+                array[i+j]=kelime2[j];
+        i++;
+    }
+    printf("\n");
+    puts(array);
+    return 0;
 }
