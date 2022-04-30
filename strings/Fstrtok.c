edec70b90c7372002b4ffd84caeb91e3e35a9c6e
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-04-30 23:57:30 +0300

    .

--- strings/Fstrtok.c
+++ strings/Fstrtok.c
@@ -1,18 +1,9 @@
 /* strtok deneem */
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-struct d
-{
-    char name[20];
-    struct d *next;
-};
-struct d *head;
 int main()
 {
-    head=(struct d*)malloc(sizeof(struct d));
-    if(head->next==NULL) printf("yeah");
-    /*char a[]="bu gun gunlerden pazartesi :)";
+    char a[]="bu gun gunlerden pazartesi :)";
     char *p;
     int i,k=0;
     p=strtok(a," ");
@@ -22,5 +13,5 @@ int main()
         p=strtok(NULL," ");
         printf("\n%s",p);
     }   
-    */return 0;
+    return 0;
 }
