185a69bf4d14fc46e89dff17404273f4d4b9383f
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-05-08 16:20:13 +0300

    .

--- lists/delete_fail.c
+++ lists/delete_fail.c
@@ -15,8 +15,25 @@ float avarege(std*);
 int main()
 {
     head=(std*)malooc(sizeof(std));//keşkem iyi ki'm işte bu yüzden her şeyim. //amma abattım :)
-    printf("\nöğrenci listesini giriniz\ndurdurmak icin -1 giriniz\n");
+    do
+    {
+        printf("\nyeni ogrenci eklemek icin 1\nlistdeki ogrenci sayısını ögrenmek icin 2\nortalamayı ogrenmek icin 3\ncıkmak icin 4\n");
+        scanf("%d",&)
+
+    } while (p!=4);
+    
+    
     add_node();    
 
 }
 void add_node()
+{
+    int
+    printf("durdurmak için -1 giriniz");
+    p->next=(std*)malloc(sizeof(std));
+    p=p->next;
+    printf("\nname surname  ");
+    scanf("%s%s"p->name,p->surname);
+    printf("midterm final  ");
+    scanf("%d%d",&p->mid,&p->final);
+}
