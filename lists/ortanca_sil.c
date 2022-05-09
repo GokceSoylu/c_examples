4f6eb01d9ad0c13cc9c74b5cb2cae62c2f66dc76
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-05-09 22:47:02 +0300

    wrong

--- lists/ortanca_sil.c
+++ lists/ortanca_sil.c
@@ -14,7 +14,9 @@ void delete_node(char s[]);
 void show_list(void);
 void search(char s[]);
 int numberOf_list(void);
-void delete_median(void);
+void deleteAdd_median(void);//listenin ortanca elemanını kesip başa ekler
+void delete_median(void);//listenin ortanca elemanını siler
+void deleteAdd_median2(void);//listenin ortanca elemanını siler başa ve sona ekler
 int main()
 {
     head=(cty*)malloc(sizeof(cty));
@@ -23,7 +25,7 @@ int main()
     char s[20];
     do
     {
-        printf("\nadd_node 1\nshow list 2\ndelete city 3\ndelete median city 4\nnumber pf city 5\nexit 6  ");
+        printf("\nadd_node 1\nshow list 2\ndelete city 3\ndelete median city and add it beginning 4\ndelete median city and add it beginning and lastd 5\nelete median city 6\nnnumber pf city 7\nexit 8  ");
         scanf("%d",&process);
         switch(process)
         {
@@ -41,24 +43,30 @@ int main()
                 delete_node(s);
                 break;
             case 4:
-                delete_median();
+                deleteAdd_median();
                 break;
             case 5:
+                deleteAdd_median2();
+                break;
+            case 6:
+                delete_median();
+                break;
+            case 7:
                 printf("%d",numberOf_list());
                 break;
         }
-    } while (process!=6);
+    } while (process!=8);
     return 0;
 }
 void add_node(char s[])
 {
-    p->next=(cty*)malloc(sizeof(cty));//head ile başlattın sonra nexte yer açıp nexte geçtin. sıralı sırasız her şkş yöntemde de head'i boş bırakıyoruz.
+    p->next=(cty*)malloc(sizeof(cty));//head ile başlattın sonra next'e yer açıp next'e geçtin. sıralı sırasız her iki yöntemde de head'i boş bırakıyoruz.
     p=p->next;
     strcpy(p->name,s);
 }
 void show_list(void)
 {
-    q=head;
+    q=head->next;
     while(q!=NULL)
     {
         printf("\n%s",q->name);
@@ -104,4 +112,39 @@ void delete_median(void)
     for(i=0;i<m;i++)
         q=q->next;
     delete_node(q->name);
+}
+void deleteAdd_median(void)
+{
+    int i, m;
+    cty *p=(cty*)malloc(sizeof(cty));
+    m=numberOf_list();
+    if(m%2!=0) m=m/2+1;
+    else m=m/2;
+    q=head;
+    for(i=0;i<m;i++)
+        q=q->next;
+    p=q;
+    delete_node(q->name);
+    p->next=head->next;
+    head->next=p;
+}
+void deleteAdd_median2(void)
+{
+    int i, m;
+    cty *p=(cty*)malloc(sizeof(cty)),*pp=(cty*)malloc(sizeof(cty));
+    m=numberOf_list();
+    if(m%2!=0) m=m/2+1;
+    else m=m/2;
+    q=head;
+    for(i=0;i<m;i++)
+        q=q->next;
+    p=pp=q;
+    delete_node(q->name);
+    p->next=head->next;
+    head->next=p;
+    q=head;
+    while(q->next!=NULL)
+        q=q->next;    
+    q->next=pp;
+    pp->next=NULL;
 }
