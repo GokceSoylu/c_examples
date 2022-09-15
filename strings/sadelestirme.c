6e287899d6687388203b02478e1c9c25cb812fc6
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-15 11:04:34 +0300

    .

--- strings/sadelestirme.c
+++ strings/sadelestirme.c
@@ -1,10 +1,22 @@
-/* 5552211221 dizisininde arka arkaya tekrar eden elemanları sadeştiriniz */
-#include <stdio.h>//I think good
+/* 5552211221 dizisininde arka arkaya tekrar eden elemanları sadeştirir */
+
+#include <stdio.h>
+
 void kaydirma(char[],int);
+void sadelestirme(char []);
+
 int main()
 {
     int i=0;
     char array[]="5552211221";
+    sadelestirme(array);
+    puts(array);
+    return 0;
+}
+
+void sadelestirme(char array[])
+{
+    int i;
     while(array[i]!='\0')
     {
         if(array[i]==array[i+1])
@@ -12,14 +24,9 @@ int main()
         else
             i++;
     }
-    puts(array);
-    return 0;
 }
 void kaydirma(char a[],int i)
 {
-    while(a[i]!='\0')
-    {
-        a[i]=a[i+1];
-        i++;
-    }    
+    for(;a[i]!='\0';i++)
+        a[i]=a[i+1];    
 }
