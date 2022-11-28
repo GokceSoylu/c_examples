2f5fef1089b04423eba997d2d8262f0e4f9ce05c
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-11-28 04:28:02 +0300

    .

--- for.c
+++ for.c
@@ -4,16 +4,20 @@ int main()
     int i,j,k, t=0, n=3;
     for(i=1;i<n;i++)
     {
+        printf("\ni = %d",i);
         for(j=1;j<n;j++)
         {
+            printf("j = %d",j);
             for(k=1;k<n;k++)
             {
                 t=t+i+j*k;
+                printf("\nk = %d\nt = %d",k,t);
             }
             j=j+1;
         }
         t=t+i*i;
+        printf("\nt = %d\n\n",t);
     }
-    printf("%d",t);
+    printf("\n\n\nsonuc = %d",t);
     return 0;
 }
