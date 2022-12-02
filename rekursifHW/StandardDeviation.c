edbcac72defc8d08aee1e6bf637c1d616b10df7f
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-02 22:28:30 +0300

    calculate standard deriavation and variance

--- rekursifHW/StandardDeviation.c
+++ rekursifHW/StandardDeviation.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-float standardDeviationSum(int[],int,float);
-float standardDeviation(float, int);
 float ort(int[],int);
+float varyance(int[],int,int,float);
+float standardDerivation(int[], int);
 int main()
 {
-    int arr[10]={1,20,30,40,1,5,200,60,120,900};//sum=1377 ort=137.7 med=35
-    printf("\n%f",sqrt(standardDeviation(standardDeviationSum(arr,10,ort(arr,10)),10)));
+    int arr[6]={12,15,20,30,45,22};
+    printf("\n%0.2f",ort(arr,6));
+    printf("\nvaryance = %0.2f",varyance(arr,5,6,ort(arr,6)));
+    printf("\nstandard derivation = %0.2f",standardDerivation(arr,6));
     return 0;
-}
-float StandardDeviation(float sd,int n)
-{
-    return (float)sd/n;
-}
-float standardDeviationSum(int arr[], int indis, float ort)
-{
-    if(indis==-1) return 0;
-    return pow((arr[indis]-ort),2)+standardDeviationSum(arr,indis-1,ort);
 }
 float ort(int arr[], int indis)
 {
     int i, ort=0;
     for(i=0;i<indis;i++) ort+=arr[i];
         return (float)ort/indis;
+}
+float varyance(int arr[], int indis, int i,float ort)
+{
+    if(indis==-1) return 0;
+    return (float)pow((arr[indis]-ort),2)/i+varyance(arr,indis-1,i,ort);
+}
+float standardDerivation(int arr[],int n)
+{
+    return (float)sqrt(varyance(arr,n-1,n,ort(arr,n)));
 }
