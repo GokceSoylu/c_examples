910ac220bbee78eafe8e9075eb1180ff94efa31d
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-19 04:26:09 +0300

    good question :)

---
+++ labFor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, m=1, key=3;  
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<7;j++)
+            if(j==key)
+            {
+                for(m=0;m<i+1;m++)
+                    printf("%c",65+m);
+                for(m=i-1;m>=0;m--)
+                    printf("%c",65+m);
+                key--;
+            }
+            else
+                printf(" ");
+        printf("\n");
+    }    
+    return 0;
+}
